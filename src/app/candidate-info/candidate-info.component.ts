b4eff84bf151aa7b05793bd78f6ef686a298f3cf
sampath kumar <sampathkumar.nuthalapathi-ext@osram-os.com>
2019-08-20 16:17:26 +0800

    basic changes

---
+++ src/app/candidate-info/candidate-info.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Candidate } from '../model/candidate';
+
+@Component({
+  selector: 'app-candidate-info',
+  templateUrl: './candidate-info.component.html',
+  styleUrls: ['./candidate-info.component.scss']
+})
+export class CandidateInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
